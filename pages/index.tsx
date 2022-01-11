2224875f53fa3a599c77e3911fb06f3ce9f95e07
RainboWZRD <bucciolsimone@gmail.com>
2022-01-11 22:20:42 +0100

    style : <GlobalStyle />

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { theme } from '../theme/theme'
+import Layout from '../theme/globalStyle'
 
 const Home: NextPage = () => {
-  return ( <>
-  <Head>
-    <title>Six dices game</title>
-    <meta name="Six dices game" content="Six dices game" />
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-  <div>Hello World</div>
+  return (<>
+    <Layout>
+      <Head>
+        <title>Six dices game</title>
+        <meta name="Six dices game" content="Six dices game" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>hello</div>
+    </Layout>
   </>
   )
 }
--- theme/theme.tsx
+++ theme/theme.tsx
@@ -1,16 +1,16 @@
-import {DefaultTheme} from 'styled-components';
+import { DefaultTheme } from 'styled-components';
 
-const theme : DefaultTheme = {
-    colors : {
-        text : "#f2f2f2",
-        background : "#141516",
-        containers : "#343536",
-        button : "#359AFF",
+const theme: DefaultTheme = {
+    colors: {
+        text: "#f2f2f2",
+        background: "#141516",
+        containers: "#343536",
+        button: "#359AFF",
     },
-    details : {
-        boxShadow : "0px 3px 6px rgba(0, 0, 0, 0.4)",
-        borderRadius : "15px",
+    details: {
+        boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.4)",
+        borderRadius: "15px",
     }
 }
 
-export {theme};
+export { theme };
