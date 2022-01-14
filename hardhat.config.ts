1ca031e186b9b44dbda976d901403961d8961969
RainboWZRD <bucciolsimone@gmail.com>
2022-01-14 09:28:38 +0100

    config: endpoints for testnets

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,6 +11,14 @@ const config: HardhatUserConfig = {
     ethereum : {
       url: process.env.INFURA_ETH_MAINNET,
       accounts: [process.env.PRIVATE_KEY!],
+    },
+    bsc : {
+      url: process.env.MORALIS_BSC_TESTNET,
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+    mumbay : {
+      url : process.env.INFURA_POLYGON_TESTNET,
+      accounts: [process.env.PRIVATE_KEY!],
     }
   },
 };
